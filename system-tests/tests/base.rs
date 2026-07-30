470e6299af16d40263f067a1ea4a821f545c466e
Manuel Hatzl <manuel.hatzl@ferrous-systems.com>
2026-07-30 12:51:01 +0200

    Add second integration test

--- system-tests/tests/base.rs
+++ system-tests/tests/base.rs
@@ -3,7 +3,7 @@ use std::{
     time::Duration,
 };
 
-use embsinth::connection::Connection;
+use embsinth::{connection::Connection, logger::test_case_end};
 use mantra_macros::{assert_req, req_verified};
 
 fn init(sim_path: &Path) -> (Connection, Connection) {
@@ -31,6 +31,8 @@ fn init(sim_path: &Path) -> (Connection, Connection) {
 #[embsinth::test]
 #[req_verified("rad.sw.operation")]
 fn integration_test() {
+    log::warn!("In integration test 1");
+
     let crate_dir =
         PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("Tests must be run via cargo"));
     let workspace_dir = crate_dir.parent().expect("Parent path must exist");
@@ -83,7 +85,62 @@ fn integration_test() {
             .is_some(),
         "Start-stop simulation did not succeed"
     );
+}
 
-    // rad_connection.close();
-    // sim_connection.close();
+#[embsinth::test]
+fn integration_test_2() {
+    log::warn!("In integration test 2");
+
+    let crate_dir =
+        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("Tests must be run via cargo"));
+    let workspace_dir = crate_dir.parent().expect("Parent path must exist");
+
+    let (mut rad_connection, mut sim_connection) =
+        init(&workspace_dir.join("target/thumbv7em-none-eabihf/debug/start_stop_flow"));
+
+    assert_req!("rad.sw.operation.start" =>
+        rad_connection
+            .search_msg_for(Duration::from_secs(10), |msg| {
+                msg.message.starts_with("Start requested")
+            })
+            .is_some(),
+        "No start was requested"
+    );
+
+    assert_req!("rad.sw.operation.pre-condition" =>
+        rad_connection
+            .search_msg_for(Duration::from_secs(5), |msg| {
+                msg.message.starts_with("Switching into 'operation' mode")
+            })
+            .is_some(),
+        "Rad did not switch into 'operation' mode"
+    );
+
+    assert_req!("rad.sw.operation.stop" =>
+        rad_connection
+            .search_msg_for(Duration::from_secs(5), |msg| {
+                msg.message.starts_with("Stop requested")
+            })
+            .is_some(),
+        "No stop was requested"
+    );
+
+    assert_req!("rad.sw.operation.post-condition" =>
+        rad_connection
+            .search_msg_for(Duration::from_secs(5), |msg| {
+                msg.message.starts_with("Switching into 'idle' mode")
+            })
+            .is_some(),
+        "Rad did not go back to 'idle'"
+    );
+
+    assert_req!("rad.sw.operation" =>
+        sim_connection
+            .search_msg_for(Duration::from_secs(5), |msg| {
+                msg.message
+                    .starts_with("RAD start/stop flow done -> exiting")
+            })
+            .is_some(),
+        "Start-stop simulation did not succeed"
+    );
 }
